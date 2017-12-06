8217e551e80fa9b81123200d4c4e0d9df24c5b48
James Hurt <jah3xc@mail.missouri.edu>
2017-12-06 16:09:29 -0600

    more work

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+
 class BudgetTable extends React.Component {
     constructor(props) {
         super(props);
@@ -67,9 +68,34 @@ class Button extends React.Component {
     }
 }
 
+
+class AddTransaction extends React.Component {
+    render() {
+        <div class="modal fade">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Modal body text goes here.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+}
+
 class BudgetControls extends React.Component {
     addTransaction() {
-        alert("Add")
+        alert("ADD")
     }
 
     deleteTransaction() {
@@ -89,6 +115,7 @@ class BudgetControls extends React.Component {
         )
     }
 }
+
 ReactDOM.render(
     <BudgetTable />,
     document.getElementById('root')
